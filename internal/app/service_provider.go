2605ffefcc3c83eb0fc8ddc9bab6afbdf18960f5
Rynk Artur <artur4502@mail.ru>
2024-09-16 13:04:51 +0300

    fixed

--- internal/app/service_provider.go
+++ internal/app/service_provider.go
@@ -4,6 +4,7 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 	"server/internal/api/handlers"
 	"server/internal/repo"
+	"server/internal/repo/internaldb"
 	"server/internal/repo/psql"
 	hs "server/internal/service/handler"
 	"server/internal/service/token"
@@ -26,18 +27,22 @@ func newServiceProvider(db *pgxpool.Pool) *serviceProvider {
 	return &serviceProvider{db: db}
 }
 
-func (s *serviceProvider) repo() (repo.User, repo.Post, repo.Comment) {
-	//s.postRepo = internaldb.NewPost()
-	//s.commentRepo = internaldb.NewComment()
-	//s.userRepo = internaldb.NewUser()
+func (s *serviceProvider) postgresRepo() (repo.User, repo.Post, repo.Comment) {
 	s.postRepo = psql.NewPost(s.db)
 	s.commentRepo = psql.NewComment(s.db)
 	s.userRepo = psql.NewUser(s.db)
 	return s.userRepo, s.postRepo, s.commentRepo
 }
 
+func (s *serviceProvider) internalRepo() (repo.User, repo.Post, repo.Comment) {
+	s.postRepo = internaldb.NewPost()
+	s.commentRepo = internaldb.NewComment()
+	s.userRepo = internaldb.NewUser()
+	return s.userRepo, s.postRepo, s.commentRepo
+}
+
 func (s *serviceProvider) serviceImpl() (users.Service, token.Service, hs.Service) {
-	s.usersS = users.New(s.repo())
+	s.usersS = users.New(s.postgresRepo())
 	s.tokenS = token.New()
 	s.handlerS = hs.New(s.usersS, s.tokenS)
 	return s.usersS, s.tokenS, s.handlerS
